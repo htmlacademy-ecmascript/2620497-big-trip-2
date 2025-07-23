bb03160078db5c3b3954d37581eca3d1c1d14811
Litvinov Ilya <dreameprogrammer82@gmail.com>
2025-07-23 17:28:32 +0300

    Шаблонизируй это

---
+++ src/const.js
@@ -0,0 +1,3 @@
+export const EVENT_TYPES = ['taxi', 'sightseeing', 'check-in', 'flight'];
+export const TIME_FORMATS = {TIME: 'HH:mm', DAY: 'MMM D', FULL_DATE: 'D/MM/YY HH:mm', TIME_TAG_VALUE: 'YYYY-MM-DD'};
+export const TIME_MESURES = ['H', 'D', 'M'];
--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -6,23 +6,43 @@ import EditFormView from '../view/edit-form-view.js';
 import TripInfoView from '../view/trip-info-view.js';
 import { render, RenderPosition } from '../render.js';
 
+import CreateFormView from '../view/create-form-view.js';
+import PointsModel from '../model/points-model.js';
+import OffersModel from '../model/offers-model.js';
+import DestinationsModel from '../model/destinations-model.js';
+
 export default class Presenter {
   constructor({ tripMainElement, filterContainer, eventsContainer }) {
     this.tripMainElement = tripMainElement;
     this.filterContainer = filterContainer;
     this.eventsContainer = eventsContainer;
+    this.pointsModel = new PointsModel();
+    this.offersModel = new OffersModel();
+    this.destinationsModel = new DestinationsModel();
     this.eventListComponent = new EventListView();
   }
 
   init() {
+    this.points = [...this.pointsModel.getPoints()];
+    this.offers = [...this.offersModel.getOffers()];
+    this.destinations = [...this.destinationsModel.getDestinations()];
     render(new TripInfoView(), this.tripMainElement, RenderPosition.AFTERBEGIN);
     render(new FilterView(), this.filterContainer);
     render(new SortView(), this.eventsContainer);
     render(this.eventListComponent, this.eventsContainer);
-    render(new EditFormView(), this.eventListComponent.getElement(), RenderPosition.AFTERBEGIN);
+    render(new CreateFormView(), this.eventListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new PointView(), this.eventListComponent.getElement(), RenderPosition.BEFOREEND);
+    if (this.points.length > 0) {
+      const point = this.points[0];
+      const offersByType = this.offers.find((offer) => offer.type === point.type)?.offers || [];
+      const destination = this.destinations.find((dest) => dest.name === point.destination);
+      const editForm = new EditFormView(point, offersByType, destination);
+      render(editForm, this.eventListComponent.getElement());
+    }
+    for (let i = 0; i < this.points.length - 1; i++) {
+      const offersByType = this.offers.find((offer) => offer.type === this.points[i].type)?.offers || [];
+      const routePoint = new PointView(this.points[i], offersByType, this.destinations[i]);
+      render(routePoint, this.eventListComponent.getElement());
     }
   }
 }
